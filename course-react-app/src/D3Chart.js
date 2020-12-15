b717a33595949ad797a807a1d8f57475c2e4091a
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-12-15 16:10:06 -0300

    - Using D3 data binding methods with a simple set of rects - Removing package-lock.json from git

--- course-react-app/src/D3Chart.js
+++ course-react-app/src/D3Chart.js
@@ -1,8 +1,25 @@
+import * as d3 from 'd3';
+
+const data = [20, 15, 26, 35, 10];
 
 export default class D3Chart {
 
     constructor(element) {
-        console.log('Hello from D3Chart!');
+        const svg = d3.select(element)
+                        .append("svg")
+                            .attr("width", 500)
+                            .attr("height", 500);
+        
+        const rects = svg.selectAll("rect")
+            .data(data);
+
+        rects.enter()
+            .append("rect")
+                .attr("x", (d, i) => i * 100)
+                .attr("y", 50)
+                .attr("height", d => d)
+                .attr("width", 50)
+                .attr("fill", d => d > 15 ? "gray" : "red");
     }
 
     update() {
